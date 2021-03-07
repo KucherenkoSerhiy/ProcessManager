50fdb165bd3527e01e4c94e539587138c7960404
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2021-03-07 15:17:08 +0100

    fixed folder structure organization

---
+++ src/API/ProcessManager.API/Controllers/ProcessController.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ProcessManager.API.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+        public ProcessController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = new List<WeatherForecast>();
+            return result;
+        }
+    }
+}
